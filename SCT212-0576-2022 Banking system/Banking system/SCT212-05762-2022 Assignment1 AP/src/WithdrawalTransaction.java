4b7563431b6efc3121f4f1dc806f09a8a935cee7
Loren Odhiambo Anguka <148061672+lorence56@users.noreply.github.com>
2025-01-20 12:50:50 +0300

    Update WithdrawalTransaction.java
    Updated with specifications

--- SCT212-0576-2022 Banking system/Banking system/SCT212-05762-2022 Assignment1 AP/src/WithdrawalTransaction.java
+++ SCT212-0576-2022 Banking system/Banking system/SCT212-05762-2022 Assignment1 AP/src/WithdrawalTransaction.java
@@ -1,11 +1,28 @@
+/**
+Overview:
+ * The WithdrawalTransaction class represents a withdrawal transaction applied 
+ * to a bank account. It extends the BaseTransaction class and implements the 
+ * apply and reverse methods to handle withdrawals and their potential reversals.
+    **/
 import java.util.Calendar;
 
 public class WithdrawalTransaction extends BaseTransaction {
+    
     private boolean reversed = false;
+      /**
+     * Constructor to initialize a WithdrawalTransaction with amount, date, and transaction ID.
+     * @param amount The monetary value of the withdrawal.
+     * @param date The date of the withdrawal transaction.
+     * @param transactionID The unique identifier for the withdrawal transaction.
+     */
 
     public WithdrawalTransaction(double amount, Calendar date, String transactionID) {
         super(amount, date, transactionID);
     }
+     /**
+     * Applies the withdrawal to the specified bank account if sufficient funds are available.
+     * @param ba The bank account from which the amount is withdrawn.
+     */
 
     @Override
     public void apply(BankAccount ba) {
@@ -16,7 +33,10 @@ public class WithdrawalTransaction extends BaseTransaction {
         ba.withdraw(getAmount());
         System.out.println("Withdrawn: " + getAmount());
     }
-
+/**
+     * Reverses the withdrawal by depositing the amount back into the bank account if not already reversed.
+     * @param ba The bank account to which the reversal applies.
+     */
     @Override
     public void reverse(BankAccount ba) {
         if (!reversed) {
