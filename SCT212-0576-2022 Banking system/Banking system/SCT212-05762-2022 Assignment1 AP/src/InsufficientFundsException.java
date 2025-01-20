e93145eadafed95f0ffad46339848e8be8d7761f
Loren Odhiambo Anguka <148061672+lorence56@users.noreply.github.com>
2025-01-20 12:33:57 +0300

    Update InsufficientFundsException.java
    Updated codes with specifications

--- SCT212-0576-2022 Banking system/Banking system/SCT212-05762-2022 Assignment1 AP/src/InsufficientFundsException.java
+++ SCT212-0576-2022 Banking system/Banking system/SCT212-05762-2022 Assignment1 AP/src/InsufficientFundsException.java
@@ -1,5 +1,14 @@
-
+/**
+ Overview:
+ * The InsufficientFundsException class is a custom exception that indicates
+ * an error condition where a transaction cannot proceed due to insufficient
+ * funds in a bank account.
+     **/
 public class InsufficientFundsException extends Exception {
+     /**
+     * Constructor to initialize an InsufficientFundsException with a custom message.
+     * @param message The error message associated with the exception.
+     */
     public InsufficientFundsException(String message) {
         super(message);
     }
